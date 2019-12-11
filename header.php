b0361be000d742ab62b31f5dbb6a99935e0d168b
Leul Mebratu <leulmebratu@gmail.com>
2019-12-11 15:37:29 +0300

    bootstrap fix

--- header.php
+++ header.php
@@ -44,7 +44,8 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
+		<nav id="site-navigation"  class="navbar navbar-expand-md navbar-dark top bg-dark">
+		
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'leulstheme' ); ?></button>
 			<?php
 			wp_nav_menu( array(
