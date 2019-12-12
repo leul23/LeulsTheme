9e65124abb9b51a01e70589a634011dcc0b48911
Leul Mebratu <leulmebratu@gmail.com>
2019-12-12 11:02:50 +0300

    Update header.php

--- header.php
+++ header.php
@@ -24,6 +24,19 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'leulstheme' ); ?></a>
 
+	<nav id="site-navigation"  class="navbar navbar-expand-md navbar-dark top bg-dark">
+		<a class="navbar-brand" href="#">
+				<?php bloginfo('name'); ?>
+		</a>
+		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'leulstheme' ); ?></button>
+		<?php
+		wp_nav_menu( array(
+			'theme_location' => 'menu-1',
+			'menu_id'        => 'primary-menu',
+		) );
+		?>
+	</nav><!-- #site-navigation -->
+
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
@@ -44,16 +57,6 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation"  class="navbar navbar-expand-md navbar-dark top bg-dark">
-		
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'leulstheme' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
