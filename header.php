9db2f49bf0574c6879216099d6925b92b92a9c0c
Leul Mebratu <leulmebratu@gmail.com>
2019-12-17 12:28:59 +0300

    Update header.php

--- header.php
+++ header.php
@@ -45,22 +45,15 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+			<br>
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$leulstheme_description = get_bloginfo( 'description', 'display' );
-			if ( $leulstheme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $leulstheme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			
+			// $leulstheme_description = get_bloginfo( 'description', 'display' );
+			// if ( $leulstheme_description || is_customize_preview() ) :
+			// 	?>
+			<!-- // 	<p class="site-description"><//?php echo $leulstheme_description; /* WPCS: xss ok. */ ?></p> -->
+			<?php //endif; ?>
 		</div><!-- .site-branding -->
 
 	</header><!-- #masthead -->
