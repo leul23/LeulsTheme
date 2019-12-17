302da06bc07111f72681a445c4da4cfe9e5dbf12
Leul Mebratu <leulmebratu@gmail.com>
2019-12-17 10:46:28 +0300

    sidebar update

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 <div class="container">
 	<div class="row">
-		<div class="col-lg-6">
+		<div class="col-sm-8">
 
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main">
@@ -57,7 +57,7 @@ get_header();
 				</main><!-- #main -->
 			</div><!-- #primary -->
 		</div>
-		<div class="col-lg-6">
+		<div class="col-sm-4">
 		<?php get_sidebar(); ?>
 		</div>
 	</div>
