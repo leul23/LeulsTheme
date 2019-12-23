259e4cbe3209437e6c51de23685b4fcd06929c8a
Leul Mebratu <leulmebratu@gmail.com>
2019-12-23 11:02:49 +0300

    folder structure

--- functions.php
+++ functions.php
@@ -156,17 +156,17 @@ add_action( 'widgets_init', 'leulstheme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function leulstheme_scripts() {
-	wp_enqueue_style( "newcss", get_template_directory_uri().'/css/newcss.css', array(), '1.0', 'all' );
+	wp_enqueue_style( "newcss", get_template_directory_uri().'/assets/css/newcss.css', array(), '1.0', 'all' );
 
-	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
 		
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array( 'jquery' ) );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array( 'jquery' ) );
 
 	wp_enqueue_style( 'leulstheme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'leulstheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'leulstheme-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'leulstheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'leulstheme-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
