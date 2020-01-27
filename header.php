8a6bf32539580ab390c06ff6110cc807bc004c04
Leul Mebratu <leulmebratu@gmail.com>
2020-01-27 13:01:19 +0300

    Nav color

--- header.php
+++ header.php
@@ -27,13 +27,13 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'theleul' ); ?></a>
 	<nav id="site-navigation"  class="navbar navbar-expand-md navbar-light top ">
 		<a class="navbar-brand" href="#"><?php bloginfo('name'); ?></a>
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'theleul' ); ?></button>
+		<button class="navbar-toggler navbar-toggler-icon navbar-toggler-right bg-light" type = "button" data-toggle="collapse" data-target="#bs4navbar" aria-controls = "bs4navbar" aria-expanded="false" aria-label="Toggle navigation"><?php esc_html_e( '', 'theleul' ); ?></button>
 		<?php
 		wp_nav_menu( array(
 			'theme_location'  => 'menu-1',
 			'menu_id'         => 'primary-menu',
 			'container'       => 'div',
-            'container_id'    => 'navbarCollapse',
+            'container_id'    => 'bs4navbar',
 			'container_class' => 'collapse navbar-collapse',
 			'menu_class'      => 'navbar-nav mr-auto',
             'walker'          => new theleul_navbar()
